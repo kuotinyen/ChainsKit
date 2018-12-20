006ca77de1a8cde3fe312a4f1121e47055716c7b
kuotinyen <kuotinyen@gmail.com>
2018-12-20 23:41:50 +0800

    Add UITableView + Chains

---
+++ Source/UIKit+Chains/UITableView+Chains.swift
@@ -0,0 +1,36 @@
+//
+//  UITableView+Chains.swift
+//  ChainsKit
+//
+//  Created by TING YEN KUO on 2018/12/20.
+//
+
+import UIKit
+
+extension UITableView {
+    
+    @discardableResult
+    public func dataSource(_ dataSource: UITableViewDataSource) -> Self {
+        self.dataSource = dataSource
+        return self
+    }
+    
+    @discardableResult
+    public func delegate(_ delegate: UITableViewDelegate) -> Self {
+        self.delegate = delegate
+        return self
+    }
+    
+    @discardableResult
+    public func separatorStyle(_ separatorStyle: UITableViewCellSeparatorStyle) -> Self {
+        self.separatorStyle = separatorStyle
+        return self
+    }
+    
+    @discardableResult
+    public func allowsSelection(_ allowsSelection: Bool) -> Self {
+        self.allowsSelection = allowsSelection
+        return self
+    }
+    
+}
