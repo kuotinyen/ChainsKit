33edc3792f6084f7b23db893a38d4a2d4c311265
kuotinyen <kuotinyen@gmail.com>
2018-12-20 23:54:29 +0800

    Add UIBarButtonItem + Chains

---
+++ Source/UIKit+Chains/UIBarButtonItem+Chains.swift
@@ -0,0 +1,29 @@
+//
+//  UIBarButtonItem+Chains.swift
+//  ChainsKit
+//
+//  Created by TING YEN KUO on 2018/12/20.
+//
+
+import UIKit
+
+extension UIBarButtonItem {
+    
+    @discardableResult
+    public func title(_ title: String) -> Self {
+        self.title = title
+        return self
+    }
+    
+    @discardableResult
+    public func style(_ style: UIBarButtonItemStyle) -> Self {
+        self.style = style
+        return self
+    }
+    
+    @discardableResult
+    public func offset(_ offset: UIOffset, for barMetrics: UIBarMetrics = .default) -> Self {
+        self.setTitlePositionAdjustment(offset, for: barMetrics)
+        return self
+    }
+}
