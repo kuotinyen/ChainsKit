194f1d0eae95a7a09329eb9a20d87e5d19a13a10
kuotinyen <kuotinyen@gmail.com>
2018-12-27 22:58:05 +0800

    Add UIViewController + Chains

---
+++ ChainsKit/Classes/UIViewController+Chains.swift.swift
@@ -0,0 +1,25 @@
+//
+//  UNUserNotificationCenter+Chains.swift
+//  ChainsKit
+//
+//  Created by TING YEN KUO on 2018/12/27.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    @discardableResult
+    public func modalPresentationStyle(_ style: UIModalPresentationStyle) -> Self {
+        self.modalPresentationStyle = style
+        return self
+    }
+    
+    @discardableResult
+    public func transitioningDelegate(_ delegate: UIViewControllerTransitioningDelegate?) -> Self {
+        self.transitioningDelegate = delegate
+        return self
+    }
+    
+}
+
