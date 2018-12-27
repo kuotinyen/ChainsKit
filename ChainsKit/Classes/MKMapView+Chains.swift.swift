626f0885d450223921f65514b0572e0525d1b66d
kuotinyen <kuotinyen@gmail.com>
2018-12-27 23:06:10 +0800

    Add MKMapView + Chains

---
+++ ChainsKit/Classes/MKMapView+Chains.swift.swift
@@ -0,0 +1,49 @@
+//
+//  UNUserNotificationCenter+Chains.swift
+//  ChainsKit
+//
+//  Created by TING YEN KUO on 2018/12/27.
+//
+
+import MapKit
+
+extension MKMapView {
+    
+    @discardableResult
+    public func showsUserLocation(_ flag: Bool) -> Self {
+        self.showsUserLocation = flag
+        return self
+    }
+    
+    @discardableResult
+    public func addAnnotationOnMap(_ annotation: MKAnnotation) -> Self {
+        self.addAnnotation(annotation)
+        return self
+    }
+    
+    @discardableResult
+    public func addAnnotationsOnMap(_ annotations: [MKAnnotation]) -> Self {
+        self.addAnnotations(annotations)
+        return self
+    }
+    
+    @discardableResult
+    public func removeAnnotationOnMap(_ annotation: MKAnnotation) -> Self {
+        self.removeAnnotation(annotation)
+        return self
+    }
+    
+    @discardableResult
+    public func removeAnnotationsOnMap(_ annotations: [MKAnnotation]) -> Self {
+        self.removeAnnotations(annotations)
+        return self
+    }
+    
+    @discardableResult
+    public func showAnnotationsOnMap(_ annotations: [MKAnnotation], animated: Bool) -> Self {
+        self.showAnnotations(annotations, animated: animated)
+        return self
+    }
+    
+}
+
