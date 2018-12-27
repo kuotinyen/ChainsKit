c5c4531b33dfbb4096c352388560e5b92bebb8c2
kuotinyen <kuotinyen@gmail.com>
2018-12-27 22:59:59 +0800

    Add UITableViewCell + Chains

---
+++ ChainsKit/Classes/UITableViewCell+Chains.swift.swift
@@ -0,0 +1,19 @@
+//
+//  UNUserNotificationCenter+Chains.swift
+//  ChainsKit
+//
+//  Created by TING YEN KUO on 2018/12/27.
+//
+
+import UIKit
+
+extension UITableViewCell {
+    
+    @discardableResult
+    public func selectionStyle(_ style: UITableViewCell.SelectionStyle) -> Self {
+        self.selectionStyle = style
+        return self
+    }
+    
+}
+
