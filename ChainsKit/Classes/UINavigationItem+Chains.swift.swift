97d47d6c7bf204d9032670d1f429e35b49564620
kuotinyen <kuotinyen@gmail.com>
2018-12-27 22:55:10 +0800

    Add UINavigationItem + Chains

---
+++ ChainsKit/Classes/UINavigationItem+Chains.swift.swift
@@ -0,0 +1,32 @@
+//
+//  UNUserNotificationCenter+Chains.swift
+//  ChainsKit
+//
+//  Created by TING YEN KUO on 2018/12/27.
+//
+
+import UIKit
+
+extension UINavigationItem {
+    
+    @discardableResult
+    public func title(_ title: String) -> Self {
+        self.title = title
+        return self
+    }
+    
+    @discardableResult
+    public func leftBarButtonItem(_ item: UIBarButtonItem?) -> Self {
+        self.leftBarButtonItem = item
+        return self
+    }
+    
+    @discardableResult
+    public func rightBarButtonItem(_ item: UIBarButtonItem?) -> Self {
+        self.rightBarButtonItem = item
+        return self
+    }
+
+    
+}
+
